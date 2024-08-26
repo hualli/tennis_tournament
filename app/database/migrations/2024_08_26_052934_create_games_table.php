ef69b41d16356bac316fa15560102e17cbc614fd
Roberto Torfe <robertotorfe@gmail.com>
2024-08-26 03:14:24 -0300

    creation of games migration

---
+++ app/database/migrations/2024_08_26_052934_create_games_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('date');
+            $table->string('round');
+            $table->string('winner');
+            $table->string('loser');
+            $table->timestamps();
+
+            $table->foreignId('tournament_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
